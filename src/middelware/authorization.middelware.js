3e11995962a8b302ddcbf10cd116590ad2f3c6f2
rehabibrahim60 <rehabibrahimr22@gmail.com>
2025-04-23 21:11:07 +0200

    Update authorization.middelware.js

--- src/middelware/authorization.middelware.js
+++ src/middelware/authorization.middelware.js
@@ -1,6 +1,21 @@
-export const isAuthorized =(role)=>{
-    return async(req,res,next)=>{
-        if(req.user.role !== role) return next(new Error("you are not auhorized" ,{cause : 403}))
-        next()
-    }
-}
+export const isAuthorized = (role) => {
+    return async (req, res, next) => {
+        try {
+            // Admin: proceed if role matches
+            if (req.user.role === role) {
+                return next();
+            }
+
+            // User: allow only if accessing their own data
+            const userIdFromParam = req.params.id; // assuming you're using /users/:id route
+            if (req.user.role === 'quality_member' && req.user._id.toString() === userIdFromParam) {
+                return next();
+            }
+
+            // Otherwise unauthorized
+            return next(new Error("You are not authorized", { cause: 403 }));
+        } catch (err) {
+            return next(err);
+        }
+    };
+};
